6a853c8e50bd0133cbe2cd288de192786d9992d6
John_Romano <romanoj723@gmail.com>
2020-07-15 11:20:41 -0400

    editing components

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Form({ query, handleInputChange, handleFormSubmit }) {
+function Form({ q, handleInputChange, handleFormSubmit }) {
   return (
     <form>
       <div className="form-group">
@@ -11,20 +11,15 @@ function Form({ query, handleInputChange, handleFormSubmit }) {
           className="form-control"
           id="Title"
           type="text"
-          // ADD VALUE ATTRIBUTE AND ASSIGN QUERY FROM INPUT ARGUMENT TO IT
-          value={query}
-          // ADD PLACEHOLDER ATTRIBUTE AND ASSIGN YOUR PLACEHOLDER TEXT HERE
-          placeholder="/* ADD YOUR PLACEHOLDER TEXT HERE */"
-          // ADD NAME ATTRIBUTE AND ASSIGN THE NAME THAT MATCHES THE INPUT ARGUMENT
-          name="query"
-          // ADD ONCHANGE EVENT ATTRIBUTE IN CAMELBACK CASE AND ASSIGN INPUT CHANGE EVENT CALLBACK FROM INPUT ARGUMENT TO IT
+          value={q}
+          placeholder="Ready Player One"
+          name="q"
           onChange={handleInputChange}
           required
         />
       </div>
       <div className="pull-right">
         <button
-          // ADD ONCLICK EVENT ATTRIBUTE IN CAMELBACK CASE AND ASSIGN FORM SUBMIT EVENT CALLBACK FROM INPUT ARGUMEMT TO IT
           onClick={handleFormSubmit}
           type="submit"
           className="btn btn-lg btn-danger float-right"
