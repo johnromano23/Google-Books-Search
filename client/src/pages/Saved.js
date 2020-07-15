f714dd5efa1b9b0442bc4caa2dc3c3544e0cfee6
John_Romano <romanoj723@gmail.com>
2020-07-15 11:25:32 -0400

    fixing pages

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,34 +1,62 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import Jumbotron from "../components/Jumbotron";
 import Card from "../components/Card";
+import Form from "../components/Form";
 import Book from "../components/Book";
 import Footer from "../components/Footer";
 import API from "../utils/API";
 import { Col, Row, Container } from "../components/Grid";
 import { List } from "../components/List";
 
-const Saved = () => {
-  const [books, setBooks] = useState([]);
-  console.log('Saved');
-  useEffect(() => {
-    loadBooks();
-  }, [])
-
-  const loadBooks = () => {
-    API.getBooksDB()
-    .then(res => 
-      setBooks(res.data)
-    )
-    .catch(err => console.log(err));
+class Home extends Component {
+  state = {
+    books: [],
+    q: "",
+    message: "Search For A Book To Begin!",
   };
 
-  const handleBookDelete = id => {
-    console.log('handleBookDelete - id', id);
-    API.deleteBook(id)
-    .then(res => loadBooks())
-    .catch(err => console.log(err));;
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
   };
 
+  getBooks = () => {
+    API.getBooks(this.state.q)
+      .then((res) =>
+        this.setState({
+          books: res.data,
+        })
+      )
+      .catch(() =>
+        this.setState({
+          books: [],
+          message: "No New Books Found, Try a Different Query",
+        })
+      );
+  };
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    this.getBooks();
+  };
+
+  handleBookSave = (id) => {
+    const book = this.state.books.find((book) => book.id === id);
+
+    API.saveBook({
+      googleId: book.id,
+      title: book.volumeInfo.title,
+      subtitle: book.volumeInfo.subtitle,
+      link: book.volumeInfo.infoLink,
+      authors: book.volumeInfo.authors,
+      description: book.volumeInfo.description,
+      image: book.volumeInfo.imageLinks.thumbnail,
+    }).then(() => this.getBooks());
+  };
+
+  render() {
     return (
       <Container>
         <Row>
@@ -37,32 +65,48 @@ const Saved = () => {
               <h1 className="text-center">
                 <strong>(React) Google Books Search</strong>
               </h1>
-              <h2 className="text-center">Search for and Save Books of Interest.</h2>
+              <h2 className="text-center">
+                Search for and Save Books of Interest.
+              </h2>
             </Jumbotron>
+          </Col>
+          <Col size="md-12">
+            <Card title="Book Search" icon="far fa-book">
+              <Form
+                handleInputChange={this.handleInputChange}
+                handleFormSubmit={this.handleFormSubmit}
+                q={this.state.q}
+              />
+            </Card>
           </Col>
         </Row>
         <Row>
           <Col size="md-12">
-            <Card title="Saved Books" icon="download">
-              {books.length ? (
+            <Card title="Results">
+              {this.state.books.length ? (
                 <List>
-                  {books.map(book => (
+                  {this.state.books.map((book) => (
                     <Book
-                      key={book._id}
-                      title={book.title}
-                      subtitle={book.subtitle}
-                      link={book.link}
-                      authors={book.authors.join(", ")}
-                      description={book.description}
-                      image={book.image}
-                      op='delete'
-                      bookId={book._id}
-                      handleBookCallBack={handleBookDelete}
+                      key={book.id}
+                      title={book.volumeInfo.title}
+                      subtitle={book.volumeInfo.subtitle}
+                      link={book.volumeInfo.infoLink}
+                      authors={book.volumeInfo.authors.join(", ")}
+                      description={book.volumeInfo.description}
+                      image={book.volumeInfo.imageLinks.thumbnail}
+                      Button={() => (
+                        <button
+                          onClick={() => this.handleBookSave(book.id)}
+                          className="btn btn-primary ml-2"
+                        >
+                          Save
+                        </button>
+                      )}
                     />
                   ))}
                 </List>
               ) : (
-                <h2 className="text-center">No Saved Books</h2>
+                <h2 className="text-center">{this.state.message}</h2>
               )}
             </Card>
           </Col>
@@ -71,5 +115,6 @@ const Saved = () => {
       </Container>
     );
   }
+}
 
-export default Saved;
+export default Home;
